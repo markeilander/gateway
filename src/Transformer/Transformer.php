9375200048d445da9a71d1bea7e602ce1542fe49
markeilander <mark.eilander@outlook.com>
2016-12-10 18:18:59 +0000

    Apply fixes from StyleCI

--- src/Transformer/Transformer.php
+++ src/Transformer/Transformer.php
@@ -1,10 +1,9 @@
-<?php 
+<?php
 
 namespace Eilander\Gateway\Transformer;
 
 use League\Fractal\TransformerAbstract;
 
-abstract class Transformer extends TransformerAbstract {
-
-
+abstract class Transformer extends TransformerAbstract
+{
 }
